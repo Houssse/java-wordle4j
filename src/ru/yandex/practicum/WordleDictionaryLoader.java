d3bab86423127dc59e94f41ebe540c93d7607a76
Yuriy Nikolaev <yuriy.nikolaev.98@mail.ru>
2026-03-31 20:44:15 +0300

    Добавил загрузку словаря из файла с указанием кодировки UTF-8

--- src/ru/yandex/practicum/WordleDictionaryLoader.java
+++ src/ru/yandex/practicum/WordleDictionaryLoader.java
@@ -1,9 +1,18 @@
 package ru.yandex.practicum;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 /*
 этот класс содержит в себе всю рутину по работе с файлами словарей и с кодировками
     ему нужны методы по загрузке списка слов из файла по имени файла
     на выходе должен быть класс WordleDictionary
  */
 public class WordleDictionaryLoader {
+    public List<String> readingFile(String nameFile) throws IOException {
+        return Files.readAllLines(Paths.get(nameFile), StandardCharsets.UTF_8);
+    }
 }
