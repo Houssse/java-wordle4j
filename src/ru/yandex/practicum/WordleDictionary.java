0fd85138ab85f12c7e061916dcfab43ca15bc6ba
Yuriy Nikolaev <yuriy.nikolaev.98@mail.ru>
2026-03-31 21:14:50 +0300

    Реализовал WordleDictionary с фильтрацией слов по длине и заменой ё на е

--- src/ru/yandex/practicum/WordleDictionary.java
+++ src/ru/yandex/practicum/WordleDictionary.java
@@ -1,6 +1,8 @@
 package ru.yandex.practicum;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /*
 этот класс содержит в себе список слов List<String>
@@ -10,5 +12,25 @@ import java.util.List;
 public class WordleDictionary {
 
     private List<String> words;
+
+    public void setWords(List<String> words) {
+        this.words = words;
+    }
+
+    public String getWord() {
+        List<String> filtered = getWords();
+        Random random = new Random();
+        return filtered.get(random.nextInt(filtered.size()));
+    }
+
+    public List<String> getWords() {
+        List<String> filtered = new ArrayList<>();
+        for (String word : words) {
+            if (word.length() == 5) {
+                filtered.add(word.toLowerCase().replace('ё', 'е'));
+            }
+        }
+        return filtered;
+    }
 
 }
